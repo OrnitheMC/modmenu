bd0bd3f5ae0b855401f82fb80b5efacb3fec7918
moehreag <moehreag@gmail.com>
2023-12-23 20:14:58 +0100

    enable modrinth update checking

--- src/main/java/com/terraformersmc/modmenu/gui/widget/DescriptionListWidget.java
+++ src/main/java/com/terraformersmc/modmenu/gui/widget/DescriptionListWidget.java
@@ -417,6 +417,7 @@ public class DescriptionListWidget extends EntryListWidget implements ResultList
 		public void render(int index, int x, int y, int width, int height, BufferBuilder bufferBuilder, int mouseX, int mouseY, boolean hovered) {
 			if (updateTextEntry) {
 				UpdateAvailableBadge.renderBadge(x + indent, y);
+				x+=11;
 			}
 			textRenderer.drawWithShadow(text, x + indent, y, 0xAAAAAA);
 		}
