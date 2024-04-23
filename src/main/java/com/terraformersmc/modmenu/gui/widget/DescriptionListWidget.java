c70f5fec577aab90346cf69d6207d657df30fb8c
LostLuma <lilly@lostluma.net>
2024-04-23 03:58:03 +0200

    Fix credits role translation keys not being lowercased

--- src/main/java/com/terraformersmc/modmenu/gui/widget/DescriptionListWidget.java
+++ src/main/java/com/terraformersmc/modmenu/gui/widget/DescriptionListWidget.java
@@ -443,7 +443,7 @@ public class DescriptionListWidget extends EntryListWidget implements ResultList
 	private Text creditsRoleText(String roleName) {
 		// Replace spaces and dashes in role names with underscores if they exist
 		// Notably Quilted Fabric API does this with FabricMC as "Upstream Owner"
-		String translationKey = roleName.replaceAll("[\\s-]", "_");
+		String translationKey = roleName.replaceAll("[\\s-]", "_").toLowerCase();
 
 		return new TranslatableText("modmenu.credits.role." + translationKey).append(new LiteralText(":"));
 	}
