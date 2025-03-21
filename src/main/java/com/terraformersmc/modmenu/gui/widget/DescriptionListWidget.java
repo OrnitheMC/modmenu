fe2cd980daa7b45d8b18c3705901f77ad27530fb
LostLuma <lilly@lostluma.net>
2025-03-21 22:57:43 +0100

    Fix erratic scrolling after handling inputs
    Signed-off-by: Lilly Rose Berner <lilly@lostluma.net>

--- src/main/java/com/terraformersmc/modmenu/gui/widget/DescriptionListWidget.java
+++ src/main/java/com/terraformersmc/modmenu/gui/widget/DescriptionListWidget.java
@@ -91,7 +91,8 @@ public class DescriptionListWidget extends EntryListWidget {
 		if (selectedEntry != lastSelected) {
 			lastSelected = selectedEntry;
 			clear();
-			scroll(-Integer.MAX_VALUE);
+			// Prevent text jumping around
+			// scroll(-Integer.MAX_VALUE);
 			if (lastSelected != null) {
 				DescriptionEntry emptyEntry = new DescriptionEntry("");
 				int wrapWidth = getRowWidth() - 5;
