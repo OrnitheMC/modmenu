0df6dc3bf2a0c35a587628f9b903b4bdb5c8299c
Pixaurora <rina@pixaurora.net>
2025-03-21 21:06:17 -0400

    Fix mods count not displaying in title screen

--- src/main/java/com/terraformersmc/modmenu/mixin/MixinTitleScreen.java
+++ src/main/java/com/terraformersmc/modmenu/mixin/MixinTitleScreen.java
@@ -23,7 +23,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(TitleScreen.class)
+@Mixin(value = TitleScreen.class, priority = 1001)
 public abstract class MixinTitleScreen extends Screen {
 	/** button id for menu.online button */
 	private static final int ONLINE = 14;
@@ -103,7 +103,7 @@ public abstract class MixinTitleScreen extends Screen {
 			if (ModMenuConfig.EASTER_EGGS.getValue() && I18n.hasTranslation(specificKey + ".secret")) {
 				replacementKey = specificKey + ".secret";
 			}
-			return string.replace(I18n.translate(I18n.translate("menu.modded")), I18n.translate(replacementKey, count));
+			return string + I18n.translate(replacementKey, count);
 		}
 		return string;
 	}
