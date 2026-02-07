f125b8101a51ac74b8baab40e857ee593ee51aea
Space Walker <spacedoesrs@gmail.com>
2026-02-07 12:39:00 +0100

    update to latest Feather Gen1 mappings

--- src/main/java/com/terraformersmc/modmenu/mixin/MixinTitleScreen.java
+++ src/main/java/com/terraformersmc/modmenu/mixin/MixinTitleScreen.java
@@ -74,7 +74,7 @@ public abstract class MixinTitleScreen extends Screen {
 		}
 	}
 
-	@ModifyArg(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/Screen;m_0986658(II)V"), method = "init", index = 1)
+	@ModifyArg(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/Screen;setSize(II)V"), method = "init", index = 1)
 	private int adjustRealmsHeight(int height) {
 		if (ModMenuConfig.MODIFY_TITLE_SCREEN.getValue() && ModMenuConfig.MODS_BUTTON_STYLE.getValue() == ModMenuConfig.TitleMenuButtonStyle.CLASSIC) {
 			return height - 51;
