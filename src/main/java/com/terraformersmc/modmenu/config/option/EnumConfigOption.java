38347b73e313611f73a51aefae562ea4950b2283
Space Walker <spacedoesrs@gmail.com>
2026-07-01 19:28:44 +0200

    use OSL's text components

--- src/main/java/com/terraformersmc/modmenu/config/option/EnumConfigOption.java
+++ src/main/java/com/terraformersmc/modmenu/config/option/EnumConfigOption.java
@@ -1,22 +1,30 @@
 package com.terraformersmc.modmenu.config.option;
 
 import com.terraformersmc.modmenu.util.TranslationUtil;
-import net.minecraft.text.Text;
-import net.minecraft.text.TranslatableText;
+
+import net.ornithemc.osl.text.api.TextComponent;
+import net.ornithemc.osl.text.api.TextComponents;
 
 import java.util.Locale;
 
 public class EnumConfigOption<E extends Enum<E>> implements ConfigOption {
-	private final String key, translationKey;
+	private final String key;
 	private final Class<E> enumClass;
 	private final E defaultValue;
+	private final TextComponent description;
+	private final TextComponent[] valueTexts;
 
 	public EnumConfigOption(String key, E defaultValue) {
 		ConfigOptionStorage.setEnum(key, defaultValue);
+		String translationKey = TranslationUtil.translationKeyOf("option", key);
 		this.key = key;
-		this.translationKey = TranslationUtil.translationKeyOf("option", key);
 		this.enumClass = defaultValue.getDeclaringClass();
 		this.defaultValue = defaultValue;
+		this.description = TextComponents.translatable(translationKey);
+		this.valueTexts = new TextComponent[this.enumClass.getEnumConstants().length];
+		for (E value : this.enumClass.getEnumConstants()) {
+			this.valueTexts[value.ordinal()] = TextComponents.translatable(translationKey + "." + value.name().toLowerCase(Locale.ROOT));
+		}
 	}
 
 	public String getKey() {
@@ -43,13 +51,9 @@ public class EnumConfigOption<E extends Enum<E>> implements ConfigOption {
 		return defaultValue;
 	}
 
-	private static <E extends Enum<E>> Text getValueText(EnumConfigOption<E> option, E value) {
-		return new TranslatableText(option.translationKey + "." + value.name().toLowerCase(Locale.ROOT));
-	}
-
 	@Override
 	public String getValueLabel() {
-		return TranslationUtil.translateOptionLabel(new TranslatableText(translationKey), getValueText(this, getValue()));
+		return TranslationUtil.translateOptionLabel(description, valueTexts[this.getValue().ordinal()]);
 	}
 
 	@Override
